31b0954bdf6e3d1b1aeb453aaea899ff7f104f8a
sserges <agounkeserge@gmail.com>
2022-04-21 11:07:20 -0400

    Parsing requests paths

--- index.js
+++ index.js
@@ -5,11 +5,21 @@
 
 // Dependencies
 const http = require('http')
+const url = require('url')
 
 const PORT = 3000
 
 const server = http.createServer((req, res) => {
+  // Get the URL and parse it
+  const parsedUrl = url.parse(req.url, true)
+
+  // Get the path
+  const path = parsedUrl.pathname
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '')
+
   res.end('Hello World')
+
+  console.log(`Request received on path: ${trimmedPath}`)
 })
 
 server.listen(PORT, () => {
