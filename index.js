602cdbc744ffe320c8e24e72e0427b9f1d3118df
sserges <agounkeserge@gmail.com>
2022-05-15 15:29:11 -0400

    adding HTTPS Support

--- index.js
+++ index.js
@@ -5,102 +5,40 @@
 
 // Dependencies
 const http = require('http')
-const url = require('url')
-const StringDecoder = require('string_decoder').StringDecoder
+const https = require('https')
+const fs = require('fs')
 
 const config = require('./config')
+const unifiedServer = require('./unified_server')
 
-const PORT = config.port
+const HTTP_PORT = config.httpPort
+const HTTPS_PORT = config.httpsPort
 const ENV_NAME = config.envName
 
-const server = http.createServer((req, res) => {
-  // Get the URL and parse it
-  const parsedUrl = url.parse(req.url, true)
-
-  // Get the path
-  const path = parsedUrl.pathname
-  const trimmedPath = path.replace(/^\/+|\/+$/g, '')
-
-  // Get the query string as an object
-  const queryStringObject = parsedUrl.query
-
-  // Get the HTTP method
-  const method = req.method.toLowerCase()
-
-  // Get the headers as an object
-  const headers = req.headers
-
-  // Get the payload, if any
-  const decoder = new StringDecoder('utf-8')
-  let buffer = ''
-  req.on('data', (data) => {
-    buffer += decoder.write(data)
-  })
-
-  req.on('end', () => {
-    buffer += decoder.end()
-
-    // Choose the handler this request should go to
-    const chosenHandler =
-      typeof router[trimmedPath] !== 'undefined'
-        ? router[trimmedPath]
-        : handlers.notFound
-
-    // Construct the data object to send to the handler
-    const data = {
-      trimmedPath,
-      queryStringObject,
-      method,
-      headers,
-      payload: buffer,
-    }
-
-    // Route the request to the handler specified in the router
-    chosenHandler(data, (statusCode, payload) => {
-      // Use the status code called back by the handler, or default to 200
-      statusCode = typeof statusCode == 'number' ? statusCode : 200
-      // Use the payload called back by the handler, or default to empty object
-      payload = typeof payload == 'object' ? payload : {}
-
-      // Convert the payload to a string
-      const payloadString = JSON.stringify(payload)
-
-      console.log('Returning this response: ', statusCode, payloadString)
-
-      // Return the response
-      res.setHeader('Content-Type', 'application/json')
-      res.writeHead(statusCode)
-      res.end(payloadString)
-    })
-
-    // console.log(
-    //   `Request received on path: ${trimmedPath} with method ${method} and with these query string parameters`
-    // )
-    // console.log(queryStringObject)
-    // console.log('headers', headers)
-    // console.log('buffer', buffer)
-  })
+// Instantiate the HTTP server
+const httpServer = http.createServer((req, res) => {
+  unifiedServer(req, res)
 })
 
-server.listen(PORT, () => {
-  console.log(`The server is listening on port ${PORT} in ${ENV_NAME} mode`)
+// Start the HTTP server
+httpServer.listen(HTTP_PORT, () => {
+  console.log(
+    `The server is listening on port ${HTTP_PORT} in ${ENV_NAME} mode`
+  )
 })
 
-// Define the handlers
-const handlers = {}
-
-// Sample handler
-handlers.sample = (data, callback) => {
-  // Callback a http status code, and a payload object
-  callback(406, { name: 'sample handler' })
+// Instantiate the HTTPS server
+const httpsServerOptions = {
+  key: fs.readFileSync('./https/key.pem'),
+  cert: fs.readFileSync('./https/cert.pem'),
 }
+const httpsServer = https.createServer(httpsServerOptions, (req, res) => {
+  unifiedServer(req, res)
+})
 
-// Not found handler
-handlers.notFound = (data, callback) => {
-  callback(404)
-}
-
-// Define a request router
-const router = {
-  sample: handlers.sample,
-}
+// Start the HTTPS server
+httpsServer.listen(HTTPS_PORT, () => {
+  console.log(
+    `The server is listening on port ${HTTPS_PORT} in ${ENV_NAME} mode`
+  )
+})
---
+++ unified_server.js
@@ -0,0 +1,86 @@
+const url = require('url')
+const StringDecoder = require('string_decoder').StringDecoder
+
+// Define the handlers
+const handlers = {}
+
+// Sample handler
+handlers.sample = (data, callback) => {
+  // Callback a http status code, and a payload object
+  callback(406, { name: 'sample handler' })
+}
+
+// Not found handler
+handlers.notFound = (data, callback) => {
+  callback(404)
+}
+
+// Define a request router
+const router = {
+  sample: handlers.sample,
+}
+
+// All the server logic for both the http and https server
+const unifiedServer = (req, res) => {
+  // Get the URL and parse it
+  const parsedUrl = url.parse(req.url, true)
+
+  // Get the path
+  const path = parsedUrl.pathname
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '')
+
+  // Get the query string as an object
+  const queryStringObject = parsedUrl.query
+
+  // Get the HTTP method
+  const method = req.method.toLowerCase()
+
+  // Get the headers as an object
+  const headers = req.headers
+
+  // Get the payload, if any
+  const decoder = new StringDecoder('utf-8')
+  let buffer = ''
+  req.on('data', (data) => {
+    buffer += decoder.write(data)
+  })
+
+  req.on('end', () => {
+    buffer += decoder.end()
+
+    // Choose the handler this request should go to
+    const chosenHandler =
+      typeof router[trimmedPath] !== 'undefined'
+        ? router[trimmedPath]
+        : handlers.notFound
+
+    // Construct the data object to send to the handler
+    const data = {
+      trimmedPath,
+      queryStringObject,
+      method,
+      headers,
+      payload: buffer,
+    }
+
+    // Route the request to the handler specified in the router
+    chosenHandler(data, (statusCode, payload) => {
+      // Use the status code called back by the handler, or default to 200
+      statusCode = typeof statusCode == 'number' ? statusCode : 200
+      // Use the payload called back by the handler, or default to empty object
+      payload = typeof payload == 'object' ? payload : {}
+
+      // Convert the payload to a string
+      const payloadString = JSON.stringify(payload)
+
+      console.log('Returning this response: ', statusCode, payloadString)
+
+      // Return the response
+      res.setHeader('Content-Type', 'application/json')
+      res.writeHead(statusCode)
+      res.end(payloadString)
+    })
+  })
+}
+
+module.exports = unifiedServer
