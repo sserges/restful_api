d66500fe7ab9393b7767c8969ce812e0889d2e1d
sserges <agounkeserge@gmail.com>
2022-05-28 15:35:06 -0400

    hash helper added

--- config.js
+++ config.js
@@ -10,6 +10,7 @@ environments.staging = {
   httpPort: 3000,
   httpsPort: 3001,
   envName: 'staging',
+  hashingSecret: 'thisIsSecret',
 }
 
 // Production environment
@@ -17,6 +18,7 @@ environments.production = {
   httpPort: 5000,
   httpsPort: 5001,
   envName: 'production',
+  hashingSecret: 'thisIsAlsoSecret',
 }
 
 // Determine which environment was passed as a command-line argument
